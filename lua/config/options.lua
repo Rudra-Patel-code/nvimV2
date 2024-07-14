044dda03a0312b6e5c88041382344c6b8d99ac83
Rudra Patel <rudracode2004@gmail.com>
2024-07-14 19:50:08 +0530

    lazy vim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,38 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+--
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("highlight Normal ctermbg=none")
+vim.cmd("highlight NonText ctermbg=none")
+
+--  move current buffer to new tab
+
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- search settings
+
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+
+-- backspace
+
+-- clipboard
+
+-- split windows
+
+-- turn off swapfile
+opt.swapfile = false
